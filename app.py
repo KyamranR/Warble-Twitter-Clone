120e643032763647dff70df1a001f3375af9e296
Kyamran Rizayev <kyamranr@gmail.com>
2024-05-27 12:32:32 -0400

    still fixing database

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql://omyedfio:tj19j-wrQuEsm3CVAHxjlVSxoUZQQWY3@kala.db.elephantsql.com/omyedfio'))
+    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
@@ -23,7 +23,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 # toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 
